be6b93cdda758333626f41e8add9b9eaf68f7dc4
Manoj <manoj@azetech.in>
2023-08-05 14:25:23 +0530

    EEPROM Wrapper Implemented

---
+++ Modified_STM/uC_STM/base/EepromWrapper/api/EepromWrapper.h
@@ -0,0 +1,33 @@
+#ifndef _EEPROMWRAPPER_H_
+#define _EEPROMWRAPPER_H_
+
+//#define number1 0x08007000
+//#define number2 0x08007010
+//#define number3 0x08007020
+//#define number4 0x08007030
+
+typedef union
+{
+	struct
+	{
+		char MobNo[13];
+		uint8_t WriteIndicator;
+	};
+	uint64_t byte[2];
+}St;
+
+extern St Data;
+
+extern St readD;
+
+extern St AdressCpy[4];
+
+extern void EEPROMErasePage(uint32_t page);
+
+extern void EepromDeleteWrite(uint32_t number,UBYTE WrtInd[]);
+
+extern void EepromFlashMmeoryCopy();
+
+extern UBYTE FlashDataRead(uint32_t Address);
+
+#endif
---
+++ Modified_STM/uC_STM/base/EepromWrapper/src/EepromWrapper.c
@@ -0,0 +1,77 @@
+#include "Includes.h"
+#include PLATFORM_TYPES_H
+#include FLASH_EEPROM_H
+#include EEPROMWRAPPER_H
+
+
+
+
+St Data;
+St readD;
+St AdressCpy[4];
+UBYTE NumberData = 0;
+
+
+UBYTE FlashDataRead(uint32_t Address)
+{
+	readD.byte[0] = *(uint64_t *)Address;
+//	SIM_Send_Data(readD.MobNo[0]);
+//	SIM_Send_Data(readD.MobNo[1]);
+//	SIM_Send_Data(readD.MobNo[2]);
+//	SIM_Send_Data(readD.MobNo[3]);
+//	SIM_Send_Data(readD.MobNo[4]);
+//	SIM_Send_Data(readD.MobNo[5]);
+//	SIM_Send_Data(readD.MobNo[6]);
+//	SIM_Send_Data(readD.MobNo[7]);
+	
+	readD.byte[1] = *(uint64_t *)(Address + 8U);
+//	SIM_Send_Data(readD.MobNo[8]);
+//	SIM_Send_Data(readD.MobNo[9]);
+//	SIM_Send_Data(readD.MobNo[10]);
+//	SIM_Send_Data(readD.MobNo[11]);
+//	SIM_Send_Data(readD.MobNo[12]);
+//	SIM_Send_Data(readD.WriteIndicator);
+	return (UBYTE)readD.WriteIndicator;
+}
+
+
+
+void EepromFlashMmeoryCopy()
+{
+	uint32_t NumberArray[4] = {0x08007000,0x08007010,0x08007020,0x08007030};
+	UBYTE dummyRead;
+	for(UBYTE i =0;i<4; i++)
+	{
+		dummyRead = FlashDataRead(NumberArray[i]);
+		if(dummyRead == 1)
+		{
+			 AdressCpy[i] = readD;
+		}
+	}
+}
+
+void EepromDeleteWrite(uint32_t number,UBYTE WrtInd[])
+{
+	EEPROMErasePage(14);
+	uint32_t EEPROMWriteAdress = 0x08007000;
+	for(UBYTE i=0;i<4;i++)
+	{
+		if((EEPROMWriteAdress == number) || (WrtInd[i] != 1))
+		{
+			EEPROMWriteAdress = EEPROMWriteAdress+16;
+			continue;
+		}
+		EEPROMmain(EEPROMWriteAdress,AdressCpy[i].byte[0]);
+		EEPROMWriteAdress = EEPROMWriteAdress+8;
+		EEPROMmain(EEPROMWriteAdress,AdressCpy[i].byte[1]);
+		EEPROMWriteAdress = EEPROMWriteAdress+8;
+	}
+}			
+
+
+void EEPROMErasePage(uint32_t page)
+{
+	FLASH_Unlock();
+	Flash_Erase(14);
+	FLASH_Lock();
+}
